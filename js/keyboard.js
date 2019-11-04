38c25f5ac87dfe2b467737cb34fd87b02aee6829
Igor Musmen <musmen@tut.by>
2019-11-04 18:59:41 +0200

    feat: add keyboard DOM-generation

--- js/keyboard.js
+++ js/keyboard.js
@@ -1,7 +1,31 @@
-let a = 5;
-a = 51;
-let c = 45;
-c = 50;
-let d = (a + c);
-d += 1;
-a += d;
+document.body.insertAdjacentHTML('afterBegin',
+  '<div class="centralizer"><textarea id="textarea" class="textarea" rows="5" cols="50"></textarea><div id="keyboard" class="keyboard"></div></div>');
+
+const fragment = document.createDocumentFragment();
+
+for (let i = 0; i < ROWS.length; i++) {
+  const row = document.createElement('div');
+  row.classList.add('row');
+  for (let j = 0; j < ROWS[i].length; j++) {
+    const div = document.createElement('div');
+    div.classList.add('key');
+    div.classList.add(ROWS[i][j].className);
+    div.insertAdjacentHTML('afterBegin',
+      `<span class="rus hidden"><span class="caseDown hidden">${ROWS[i][j].rus.caseDown}</span><span class="caseUp hidden">${ROWS[i][j].rus.caseUp}</span></span><span class="eng"><span class="caseDown">${ROWS[i][j].eng.caseDown}</span><span class="caseUp hidden">${ROWS[i][j].eng.caseUp}</span></span>`);
+    row.appendChild(div);
+  }
+  fragment.appendChild(row);
+}
+
+keyboard.appendChild(fragment);
+
+/* <div class="key keyQ">
+  <span class="rus hidden">
+    <span class="caseDown hidden">й</span>
+    <span class="caseUp hidden">Й</span>
+  </span>
+  <span class="eng">
+    <span class="caseDown">q</span>
+    <span class="caseUp hidden">Q</span>
+  </span>
+</div> */
